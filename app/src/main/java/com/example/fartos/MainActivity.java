3edf6c4978fa56a6d1f37ac38843e6a816f8e1a5
miguelurdaneta <murdanet@jaumebalmes.net>
2023-04-09 17:18:45 +0200

    .

---
+++ app/src/main/java/com/example/fartos/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.fartos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+
+    }
+}
